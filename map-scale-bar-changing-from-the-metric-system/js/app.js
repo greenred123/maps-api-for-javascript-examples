be4af401eeba9da3352bfb730df5981a37a9db96
Langford, Chloe <chloe.langford@here.com>
2019-02-11 13:25:51 +0100

    test if credentials solve zoom on map-scale-bar-changing-from-the-metric-system example

--- map-scale-bar-changing-from-the-metric-system/js/app.js
+++ map-scale-bar-changing-from-the-metric-system/js/app.js
@@ -24,8 +24,8 @@ function useImperialMeasurements(map, defaultLayers){
 
 //Step 1: initialize communication with the platform
 var platform = new H.service.Platform({
-  app_id: '{YOUR_APP_ID}',
-  app_code: '{YOUR_APP_CODE}',
+  app_id: 'devportal-demo-20180625',
+  app_code: '9v2BkviRwi9Ot26kp2IysQ',
   useCIT: true,
   useHTTPS: true
 });
